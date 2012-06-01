a67a00cf46d307c37df79aa75313efe94cb6f255
Francis Noel <ploufs@hotmail.com>
2012-06-01 14:47:45 -0400

    first version

---
+++ IftttGithub.Test/GithubParserTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ServiceModel.Syndication;
+using System.Linq;
+using System.Net;
+using System.Xml;
+
+namespace IftttGithub.Test
+{
+    [TestClass]
+    public class GithubParserTest
+    {
+        [TestMethod]
+        public void GetRssWathFromUserName_EmptyXmlWhenUsernameNotFound()
+        {
+            var parser = new GithubParser();
+            var result = parser.GetRssWathFromUserName("dddddfewr32r4");
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Items.Count() == 0);
+        }
+
+        [TestMethod]
+        public void GetRssWathFromUserName_ReturnValue()
+        {
+            var parser = new GithubParser();
+           var result= parser.GetRssWathFromUserName("ploufs");
+
+            Assert.IsTrue(result.Items.Count() > 0);
+        }
+
+        [TestMethod]
+        public void GetRssWathFromUserName_SaveToHd()
+        {
+            var parser = new GithubParser();
+            var result = parser.GetRssWathFromUserName("ploufs");
+
+            var filename=@"c:\TestRSSFile.xml";
+
+            System.IO.File.Delete(filename);
+
+            XmlWriter xmlWriter = XmlWriter.Create(filename);
+            result.SaveAsRss20(xmlWriter);
+            xmlWriter.Close();
+            xmlWriter.Dispose();
+
+            Assert.IsTrue(System.IO.File.Exists(filename));
+
+        }
+
+        [TestMethod]
+        public void GetRssWathFromUserName_DownloadUrlIsFound()
+        {
+            var parser = new GithubParser();
+            var result = parser.GetRssWathFromUserName("ploufs");
+
+            if (result.Items.Count() > 0)
+            {
+                /*foreach (var item in result.Items)
+                {
+                    foreach (var link in item.Links)
+                    {
+                        Assert.IsTrue(this.RemoteFileExists(link.Uri.ToString())); 
+                    }    
+                }*/
+                Assert.IsTrue(this.RemoteFileExists(result.Items.FirstOrDefault().Links.FirstOrDefault().Uri.ToString())); 
+            }
+        }
+
+        /// <summary>
+        /// http://stackoverflow.com/questions/924679/c-sharp-how-can-i-check-if-a-url-exists-is-valid
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private bool RemoteFileExists(string url)
+        {
+            try
+            {
+                //Creating the HttpWebRequest
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                //Setting the Request method HEAD, you can also use GET too.
+                request.Method = "HEAD";
+                //Getting the Web Response.
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                //Returns TURE if the Status code == 200
+                return (response.StatusCode == HttpStatusCode.OK);
+            }
+            catch
+            {
+                //Any exception will returns false.
+                return false;
+            }
+        }
+    }
+}
---
+++ IftttGithub/GithubParser.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.ServiceModel.Syndication;
+using Newtonsoft.Json.Linq;
+
+namespace IftttGithub
+{
+    public class GithubParser
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="username">github account username</param>
+        /// <returns></returns>
+        public SyndicationFeed GetRssWathFromUserName(string username)
+        {
+            SyndicationFeed feed = new SyndicationFeed("Watched " + username, "Github watched" + username, null, username, DateTime.Now);
+
+            string url = string.Format("https://api.github.com/users/{0}/watched", username);
+            var json = string.Empty;
+            try
+            {
+                json = new WebClient().DownloadString(url);
+            }
+            catch (WebException ex)
+            {
+                if (((HttpWebResponse)ex.Response).StatusCode == HttpStatusCode.NotFound)
+                {
+                    json = string.Empty;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+
+            if (!string.IsNullOrWhiteSpace(json))
+            {
+                JArray Projects = JArray.Parse(json);
+
+                List<SyndicationItem> FeedItems = new List<SyndicationItem>();
+
+                foreach (var project in Projects)
+                {
+                    DateTime UpdateDate = DateTime.Parse(project["updated_at"].ToString());
+                    DateTime PushedAt = DateTime.Parse(project["pushed_at"].ToString());
+                    string id = project["id"].ToString();
+                    string full_name = project["full_name"].ToString();
+                    string name = project["name"].ToString();
+                    string Description = project["description"].ToString();
+                    string DownloadUrl = string.Format("https://github.com/{0}/zipball/master", full_name);
+
+                    SyndicationItem item = new SyndicationItem(name, Description, new Uri(DownloadUrl), id, UpdateDate);
+                    item.PublishDate = UpdateDate;
+                    FeedItems.Add(item);
+                }
+
+                feed.Items = FeedItems;
+
+            }
+
+            
+            return feed;
+        }
+
+
+    }
+}
